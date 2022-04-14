088fef3928036e33b0315d044aab7ab3928f3123
shirtol <shirzahavi7@gmail.com>
2022-04-14 23:33:46 +0300

    Added edge cases for ex4.2

--- Ex4.2- Tribonacci/app.js
+++ Ex4.2- Tribonacci/app.js
@@ -1,6 +1,21 @@
+/*
+?Assumptions:
+** signature array will always contain 3 numbers
+** n will always be a non-negative number
+*/
+
 const tribonacci = (signatureArr, order) => {
     const tribonacciSeriesArr = [...signatureArr];
     let currNum;
+    if (order === 0) {
+        return [];
+    }
+    if (order === 1) {
+        return signatureArr.slice(0, 1);
+    }
+    if (order === 2) {
+        return signatureArr.slice(0, 2);
+    }
     for (let i = 0; i < order - 3; i++) {
         currNum =
             tribonacciSeriesArr[i] +
@@ -15,3 +30,6 @@ console.log(tribonacci([1, 1, 1], 5));
 console.log(tribonacci([1, 1, 1], 8));
 console.log(tribonacci([0, 0, 1], 8));
 console.log(tribonacci([1, 2, 3], 6));
+console.log(tribonacci([1, 2, 3], 0));
+console.log(tribonacci([1, 2, 3], 1));
+console.log(tribonacci([1, 2, 3], 2));
