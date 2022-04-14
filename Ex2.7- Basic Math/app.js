20db606e843ad01ebcc6e524d17f606f06b5c21c
shirtol <shirzahavi7@gmail.com>
2022-04-14 15:37:13 +0300

    Added exercise 2.7

---
+++ Ex2.7- Basic Math/app.js
@@ -0,0 +1,19 @@
+const basicOp = (operator, val1, val2) => {
+    switch (operator) {
+        case "+":
+            return val1 + val2;
+        case "-":
+            return val1 - val2;
+        case "*":
+            return val1 * val2;
+        case "/":
+            return val1 / val2;
+        default:
+            return null;
+    }
+};
+
+console.log(basicOp("+", 4, 7));
+console.log(basicOp("-", 15, 18));
+console.log(basicOp("*", 5, 5));
+console.log(basicOp("/", 49, 7));
