f853577b2ab34ed7111999f4e4eb05106951806b
shirtol <shirzahavi7@gmail.com>
2022-04-14 15:25:47 +0300

    Added exercise 2.6

---
+++ Ex2.6- Years and Centuries/app.js
@@ -0,0 +1,6 @@
+const centuryFromYear = (year) => Math.ceil(year / 100);
+
+console.log(centuryFromYear(1705));
+console.log(centuryFromYear(1900));
+console.log(centuryFromYear(1601));
+console.log(centuryFromYear(2000));
