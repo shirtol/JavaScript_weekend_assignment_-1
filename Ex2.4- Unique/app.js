fea530f1267a5cd2cc7f9066519a5953ea3cc0b3
shirtol <shirzahavi7@gmail.com>
2022-04-14 15:05:47 +0300

    Added exercise 2.4

---
+++ Ex2.4- Unique/app.js
@@ -0,0 +1,15 @@
+const findUnique = (arr) => {
+    const counts = {};
+    for (num of arr) {
+        num = num.toString();
+        num in counts ? counts[num]++ : (counts[num] = 1);
+    }
+    return Object.values(counts)[0] === 1
+        ? Object.keys(counts)[0]
+        : Object.keys(counts)[1];
+};
+
+console.log(findUnique([1, 1, 1, 2, 1, 1]));
+console.log(findUnique([0, 0, 0.55, 0, 0]));
+console.log(findUnique([1, 0, 0, 0, 0]));
+console.log(findUnique([0, 0, 0, 0, 3]));
