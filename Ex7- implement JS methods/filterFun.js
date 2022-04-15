7ac9dd390281175e1cad71d5fec237a7bb5f9554
shirtol <shirzahavi7@gmail.com>
2022-04-15 17:14:04 +0300

    Added another filter approach

--- Ex7- implement JS methods/filterFun.js
+++ Ex7- implement JS methods/filterFun.js
@@ -1,4 +1,19 @@
-const myFilter = (callback, array) => {
+// First approach: Extension method for Array class.
+Array.prototype.myFilter = function (callback) {
+    const filteredArr = [];
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i, this)) {
+            filteredArr.push(this[i]);
+        }
+    }
+    return filteredArr;
+};
+
+const myArr = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(myArr.myFilter((el) => el > 5));
+
+// Another approach: The function gets the array as a parameter:
+const myFilter1 = (callback, array) => {
     const filteredArr = [];
     for (let i = 0; i < array.length; i++) {
         if (callback(array[i], i, array)) {
@@ -8,4 +23,4 @@ const myFilter = (callback, array) => {
     return filteredArr;
 };
 
-console.log(myFilter((el) => el > 5, [1, 2, 3, 4, 5, 6, 7, 8]));
+console.log(myFilter1((el) => el > 5, [1, 2, 3, 4, 5, 6, 7, 8]));
