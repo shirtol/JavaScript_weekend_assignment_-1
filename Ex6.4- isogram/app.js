5cda21d5a9c2e456eec1a3c48b97a071a53af23f
shirtol <shirzahavi7@gmail.com>
2022-04-15 14:18:27 +0300

    Added exercise 6.4

---
+++ Ex6.4- isogram/app.js
@@ -0,0 +1,23 @@
+/*
+Assumptions:
+? The string contains only letters.
+? Empty string is an isogram.
+? Ignore letter case.
+*/
+
+const isIsogram = (str) => {
+    let lowercaseStr = str.toLowerCase();
+    const countsArr = [];
+    for (let i = 0; i < lowercaseStr.length; i++) {
+        if (countsArr.indexOf(lowercaseStr[i]) !== -1) {
+            return false;
+        }
+        countsArr.push(lowercaseStr[i]);
+    }
+    return true;
+};
+
+console.log(isIsogram("Dermatoglyphics"));
+console.log(isIsogram("aba"));
+console.log(isIsogram("moOse"));
+console.log(isIsogram(""));
