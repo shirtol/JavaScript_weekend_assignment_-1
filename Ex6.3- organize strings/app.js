57794c7251085e4668fbad229c2bf68810179d6c
shirtol <shirzahavi7@gmail.com>
2022-04-15 13:58:19 +0300

    Added exercise 6.3

---
+++ Ex6.3- organize strings/app.js
@@ -0,0 +1,25 @@
+/*
+Assumptions:
+? str1 and str2 including only letters from a to z.
+*/
+
+const organizeStr = (str1, str2) => {
+    const distinctLettersArr = [];
+    getDistinctLettersArr(str1, distinctLettersArr);
+    getDistinctLettersArr(str2, distinctLettersArr);
+    return distinctLettersArr.sort().join("");
+};
+
+const getDistinctLettersArr = (str, distinctLettersArr) => {
+    let lowercaseStr = str.toLowerCase();
+    for (let i = 0; i < lowercaseStr.length; i++) {
+        if (distinctLettersArr.indexOf(lowercaseStr[i]) === -1) {
+            distinctLettersArr.push(lowercaseStr[i]);
+        }
+    }
+};
+
+console.log(organizeStr("xyaabbbccccdefww", "xxxxyyyyabklmopq"));
+console.log(
+    organizeStr("abcdefghijklmnopqrstuvwxyz", "abcdefghijklmnopqrstuvwxyz")
+);
