09829a2142b086152f598bb0861e8ce81189152b
shirtol <shirzahavi7@gmail.com>
2022-04-15 11:39:00 +0300

    Added exercise 5.5

---
+++ Ex5.5- Abbreviate two words/app.js
@@ -0,0 +1,15 @@
+/*
+Assumptions:
+? The input is string and contains only 2 words with one space in between them
+*/
+
+const abbreviateName = (str) => {
+    const splitedStr = str.split(" ");
+    return (
+        splitedStr[0][0].toUpperCase() + "." + splitedStr[1][0].toUpperCase()
+    );
+};
+
+console.log(abbreviateName("Sam Harris"));
+console.log(abbreviateName("Patrick Feeney"));
+console.log(abbreviateName("shir toledano"));
