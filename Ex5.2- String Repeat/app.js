5f58a24d0c56ffe4be8cf6aeacf0a3421a8d09c5
shirtol <shirzahavi7@gmail.com>
2022-04-15 10:14:50 +0300

    Added exercise 5.2

---
+++ Ex5.2- String Repeat/app.js
@@ -0,0 +1,12 @@
+const repeatStr = (counts, string) => {
+    let repeatedStr = "";
+    for (let i = 0; i < counts; i++) {
+        repeatedStr += string;
+    }
+    return repeatedStr;
+};
+
+console.log(repeatStr(6, "|"));
+console.log(repeatStr(5, "Hello"));
+console.log(repeatStr(0, "Hello"));
+console.log(repeatStr(1, "Hello"));
