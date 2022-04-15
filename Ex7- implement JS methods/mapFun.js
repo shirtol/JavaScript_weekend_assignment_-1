721a5c723194370eeeb3e7909710b1c54da5df95
shirtol <shirzahavi7@gmail.com>
2022-04-15 17:09:44 +0300

    Added map function

---
+++ Ex7- implement JS methods/mapFun.js
@@ -0,0 +1,24 @@
+// First approach: Extension method for Array class.
+Array.prototype.myMap = function (callback) {
+    const newArr = [];
+
+    for (let i = 0; i < this.length; i++) {
+        newArr.push(callback(this[i], i, this));
+    }
+
+    return newArr;
+};
+
+const myArr = [1, 2, 3, 4, 5];
+console.log(myArr.myMap((el) => el * 2));
+
+// Another approach: The function gets the array as a parameter:
+const myMap1 = (callback, array) => {
+    const newArr = [];
+    for (let i = 0; i < array.length; i++) {
+        newArr.push(callback(array[i], i, array));
+    }
+    return newArr;
+};
+
+console.log(myMap1((el) => el * 2, [1, 2, 3, 4, 5]));
