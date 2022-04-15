bd8617e2e6881a74088b70a096860e36a2478987
shirtol <shirzahavi7@gmail.com>
2022-04-15 13:06:24 +0300

    Refactoring exercise 6.1

---
+++ Ex6.1- mumbling/app.js
@@ -0,0 +1,25 @@
+/*
+I think that the examples have a mistake:
+(example number 2): accum("RqaEzty") -> "R-Qq-Aaa-Eeeezzzz-Tttttt-Yyyyyyy",
+I think it should be: accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy",
+according to the other examples and the first 2 letters of this example.
+*/
+
+const accum = (str) => {
+    const charsArr = str.split("");
+    const accumArr = [];
+
+    charsArr.forEach((char, idx) => {
+        let currChar = char.toUpperCase();
+        for (let i = 0; i < idx; i++) {
+            currChar += char.toLowerCase();
+        }
+        accumArr.push(currChar);
+    });
+
+    return accumArr.join("-");
+};
+
+console.log(accum("abcd"));
+console.log(accum("RqaEzty"));
+console.log(accum("cwAt"));
