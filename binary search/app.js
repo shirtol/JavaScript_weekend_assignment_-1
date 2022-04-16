20766793640589d7af4df796eb48b110e2088a97
shirtol <shirzahavi7@gmail.com>
2022-04-16 14:51:48 +0300

    Added binary search tests and fixed bugs

--- binary search/app.js
+++ binary search/app.js
@@ -2,7 +2,7 @@ const binarySearch = (arr, x) => {
     let l = 0;
     let r = arr.length - 1;
     let m = Math.floor(l - (l - r) / 2);
-    while (l < r) {
+    while (l <= r) {
         if (arr[m] > x) {
             r = m - 1;
         } else if (arr[m] < x) {
@@ -17,3 +17,5 @@ const binarySearch = (arr, x) => {
 
 console.log(binarySearch([1, 3, 5, 7, 8, 9], 5));
 console.log(binarySearch([1, 3, 5, 7, 8, 9], 6));
+
+module.exports = binarySearch;
---
+++ test/binary search/test.js
@@ -0,0 +1,23 @@
+const assert = require("chai").assert;
+const binarySearch = require("../../binary search/app");
+
+describe("binary search test", () => {
+    it("testing for binary search tests", () => {
+        assert.strictEqual(
+            binarySearch([1, 3, 5, 7, 8, 9], 5),
+            "Element found!"
+        );
+        assert.strictEqual(
+            binarySearch([1, 3, 5, 7, 8, 9], 6),
+            "Element not found!"
+        );
+        assert.strictEqual(
+            binarySearch([1, 3, 5, 7, 8, 9], 9),
+            "Element found!"
+        );
+        assert.strictEqual(binarySearch([1, 3, 4], 6), "Element not found!");
+        assert.strictEqual(binarySearch([1, 3, 4], 1), "Element found!");
+        assert.strictEqual(binarySearch([1, 3], 6), "Element not found!");
+        assert.strictEqual(binarySearch([1, 3], 1), "Element found!");
+    });
+});
