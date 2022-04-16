09c7a286bc18f60eec02d8c64cf1f8ff98453cce
shirtol <shirzahavi7@gmail.com>
2022-04-16 16:54:59 +0300

    Deleted irrelevant logs in Ex2.2

--- Ex2.2- One and Zero - binary/app.js
+++ Ex2.2- One and Zero - binary/app.js
@@ -6,24 +6,6 @@ const convertBinaryToInt = (binaryArr) => {
     return intValue;
 };
 
-const testing1 = [0, 0, 0, 1];
-const testing2 = [0, 0, 1, 0];
-const testing3 = [0, 1, 0, 1];
-const testing4 = [1, 0, 0, 1];
-const testing5 = [0, 0, 1, 0];
-const testing6 = [0, 1, 1, 0];
-const testing7 = [1, 1, 1, 1];
-const testing8 = [1, 0, 1, 1];
-
-console.log(convertBinaryToInt(testing1));
-console.log(convertBinaryToInt(testing2));
-console.log(convertBinaryToInt(testing3));
-console.log(convertBinaryToInt(testing4));
-console.log(convertBinaryToInt(testing5));
-console.log(convertBinaryToInt(testing6));
-console.log(convertBinaryToInt(testing7));
-console.log(convertBinaryToInt(testing8));
-
 const convertBinaryToInt2 = (binaryArr) => {
     let intVal = 0;
     binaryArr
