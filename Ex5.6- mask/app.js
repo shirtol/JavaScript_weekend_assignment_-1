9c566f66486cf6118377c9d4eeccb5b2d368bc8d
shirtol <shirzahavi7@gmail.com>
2022-04-16 16:55:32 +0300

    Deleted irrelevant logs in Ex5.6

--- Ex5.6- mask/app.js
+++ Ex5.6- mask/app.js
@@ -10,11 +10,4 @@ const maskify = (privateStr) => {
     return maskedStr + lastFourChars;
 };
 
-console.log(maskify("4556364607935616"));
-console.log(maskify("64607935616"));
-console.log(maskify("1"));
-console.log(maskify(""));
-console.log(maskify("Skippy"));
-console.log(maskify("Nananananananananananananananana Batman!"));
-
 module.exports = maskify;
