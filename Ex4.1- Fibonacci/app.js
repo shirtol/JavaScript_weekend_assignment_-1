b03064c02face76886249af2b15e935f09dfd933
shirtol <shirzahavi7@gmail.com>
2022-04-16 19:30:39 +0300

    Added more explanations to Ex4.1

--- Ex4.1- Fibonacci/app.js
+++ Ex4.1- Fibonacci/app.js
@@ -2,6 +2,7 @@
 The function gets a starting value (0 or 1) and an integer n (which is the order number of the element).
 */
 
+// Runtime complexity: O(n). Better performance than the other solutions (performance test results for the 200th element: 0.282ms).
 const fibonacciWithLoop = (startVal, order) => {
     const seriesArr = [startVal, 1];
     for (let i = 0; i < order - 2; i++) {
@@ -10,7 +11,7 @@ const fibonacciWithLoop = (startVal, order) => {
     return seriesArr[order - 1];
 };
 
-// Runtime complexity: O(2^n)
+// Runtime complexity: O(2^n). (performance test results for the 200th element: > 15min)
 const fibonacci = (startVal, order) => {
     if (order === 1 || order === 0) {
         return startVal;
@@ -21,7 +22,7 @@ const fibonacci = (startVal, order) => {
     return fibonacci(startVal, order - 1) + fibonacci(startVal, order - 2);
 };
 
-// Better performance (Runtime complexity: O(n))
+// Better performance (Runtime complexity: O(n)). (performance test results for the 200th element: 0.586ms)
 const fibonacciImproved = (startVal, order, memoObj) => {
     if (order === 1 || order === 0) {
         memoObj[order] = startVal;
