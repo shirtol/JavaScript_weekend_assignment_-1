c9b8c62a630e496265901ccec25c86b8f91bb44b
shirtol <shirzahavi7@gmail.com>
2022-04-16 13:22:17 +0300

    Added binary search solution

---
+++ binary search/app.js
@@ -0,0 +1,19 @@
+const binarySearch = (arr, x) => {
+    let l = 0;
+    let r = arr.length - 1;
+    let m = Math.floor(l - (l - r) / 2);
+    while (l < r) {
+        if (arr[m] > x) {
+            r = m - 1;
+        } else if (arr[m] < x) {
+            l = m + 1;
+        } else if (arr[m] === x) {
+            return "Element found!";
+        }
+        m = Math.floor(l - (l - r) / 2);
+    }
+    return "Element not found!";
+};
+
+console.log(binarySearch([1, 3, 5, 7, 8, 9], 5));
+console.log(binarySearch([1, 3, 5, 7, 8, 9], 6));
